cbf87829717112c4127c1687d2de96608c69dbd4
Tom-js-python <tomrembrown@proton.me>
2026-09-05 13:17:34 -0400

    Switch from Open Sans to Inter font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
   },
