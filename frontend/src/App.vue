6106833e04834dd741b5f71da73a6a472afda260
Tom-js-python <tomrembrown@proton.me>
2026-09-15 13:14:46 -0400

    Add Page Heading to Election Results view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="min-h-screen bg-slate-50 text-slate-900 dark:bg-slate-950 dark:text-slate-100">
     <app-header />
+    <router-view />
   </div>
 </template>
 
---
+++ frontend/src/views/ElectionResultsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="mx-auto max-w-screen-2xl px-4 py-6 sm:px-6 lg:px-8">
+    <page-heading />
+  </main>
+</template>
+
+<script lang="ts" setup>
+import PageHeading from '@/components/ElectionResults/PageHeading.vue'
+</script>
